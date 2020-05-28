5f520427838dbbb4ca671a630de20b2840168461
pam7462 <64447895+pam7462@users.noreply.github.com>
2020-05-28 12:31:30 +0900

    Add files via upload

--- js/ui.js
+++ js/ui.js
@@ -1,8 +1,25 @@
 $(function(){
+  // 스킵메뉴
   $('.skip>a').focus(function(){
     $('.skip').addClass('active');
   });
   $('.skip>a').blur(function(){
     $('.skip').removeClass('active');
   });
+
+  // 1066px 이상에서의 pos_fix 카카오톡 변경
+  $(window).resize(function(){
+    fnKakao();
+  });
+
+  fnKakao();
+
+  function fnKakao(){
+    var winWidth=$(this).width();
+    if(winWidth>=1066){
+      $('#kakao').text('카카오톡');
+    }else{
+      $('#kakao').text('카카오톡 상담');
+    }
+  }
 })
